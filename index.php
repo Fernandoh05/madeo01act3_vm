9b69e9d94df39504261de7a0a433c7419311c039
Juan Carlos <jc.przhdz@gmail.com>
2024-07-17 23:18:00 -0500

    agregados

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-phpinfo();
-?>
+<html>
+    <index>
+		<title>UNIR</title>
+	</index>
+    <body>
+	    <h1>Actividad 3</h1>
+		<h2>Despliegue de una aplicación mediante AWS </h2>
+        <h3>Saludos!</h3>
+	</body>
+</html>
